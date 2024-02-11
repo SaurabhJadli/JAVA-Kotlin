3c95d33a163280fc1b12b957da37661125aaf2e7
Saurabh Jadli <86057535+SaurabhJadli@users.noreply.github.com>
2024-02-11 22:56:47 +0530

    febinaci series

---
+++ Fibonaci.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val myInput = 7
+    var temp1 = 0
+    var temp2 = 1
+ 
+    println("number is: $myInput")
+    println("Fibonacci series till $myInput terms:")
+ 
+    for (i in 1..myInput) {
+       print("$temp1 ")
+       
+       val sum = temp1 + temp2
+       temp1 = temp2
+       temp2 = sum
+    }
+}
